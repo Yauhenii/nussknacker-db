71976441f90b169e3ff897179d44281affc0f582
Yauhenii <yauhenmardan@gmail.com>
2021-12-25 18:03:43 +0100

    fix sql bug

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS nussknacker;
-CREATE TABLE IN NOT EXISTS nussknacker.entity(
+CREATE TABLE IF NOT EXISTS nussknacker.entity(
     id serial PRIMARY KEY,
     e_name varchar(50),
     e_number float(10),
